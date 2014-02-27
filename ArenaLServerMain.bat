ac4ed668a01c07edd76982db0981d5b6f7900cc9
Trystan Binkley-Jones <tcbinkleyj@gmail.com>
2014-02-26 18:19:03 -0700

    More stuff

--- ArenaLServerMain.bat
+++ ArenaLServerMain.bat
@@ -1,1 +1,1 @@
-C:\UDK\UDK-2012-05\Binaries\Win32\UDK.exe ArenaFloor?listen?GoalScore=0?TimeLimit=0?Game=Arena.GI_BotBattle -log
+C:\UDK\UDK-2012-05\Binaries\Win32\UDK.exe AlphaTestingPillars?listen?GoalScore=0?TimeLimit=0?Game=Arena.GI_BotBattle -log
